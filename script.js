ff15620f5bb8aae9708095b5c078130c3c438832
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-15 15:29:26 +0200

    added a subtraction function

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const num1 = 100;
 const num2 = 175;
 
 add(num1, num2);
+subtract(num1, num2);
 
 function add(num1, num2) {
     const total = num1 + num2;
     return total;
+}
+
+function subtract(num1, num2) {
+    const difference = num2 - num1;
+    return difference;    
+}
+
+function multiply(num1, num2) {
+    
 }
 
