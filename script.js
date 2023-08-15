538faba4d9815c756a2ddf995cf0c5d72156b18a
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-15 15:36:27 +0200

    added a divide function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const num2 = 175;
 
 add(num1, num2);
 subtract(num1, num2);
+divide(num2, num1);
+multiply(num1, num2);
 
 function add(num1, num2) {
     const total = num1 + num2;
@@ -17,5 +19,10 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
     const product = num1 * num2;
     return product;
+}
+
+function divide(num2, num1) {
+    const quotient = num2 / num1;
+    return quotient;
 }
 
