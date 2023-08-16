1836afa9cd21f89dc6751786cf262b74f028dc7f
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-16 11:57:26 +0200

    selected all the buttons and used an alert to display which btn clicked

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const num1 = 10000;
 const num2 = 1750000;
+const outPut = document.querySelector('#output');
+const userInPut = document.querySelector('#user-input');
+const buttons = document.querySelectorAll('.btn');
+
 // const operator = "/";
 
 console.log(add(num1, num2));
 console.log(subtract(num1, num2));
 console.log(divide(num2, num1));
 console.log(multiply(num1, num2));
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
+        // display(value);
+        alert(value);
+    })
+})
 
 function add(num1, num2) {
     const total = num1 + num2;
