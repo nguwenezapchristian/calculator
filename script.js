f0da4eb135511cbf7c5aa9e63125e1edb037300b
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-16 22:52:46 +0200

    deleted some console.log for testing the functions

--- script.js
+++ script.js
@@ -6,22 +6,17 @@ const buttons = document.querySelectorAll('.btn');
 
 // const operator = "/";
 
-console.log(add(num1, num2));
-console.log(subtract(num1, num2));
-console.log(divide(num2, num1));
-console.log(multiply(num1, num2));
-
 buttons.forEach(function(button) {
     button.addEventListener('click', () => {
         const value = button.getAttribute('data-value');
         dealWithValue(value);
-        alert(value);
+        // alert(value);
     })
 })
 
 function dealWithValue(value) {
     if (typeof value === 'number') {
-        
+        userInPut.textContent = String(value);
     }
 }
 
@@ -44,4 +39,3 @@ function divide(num2, num1) {
     const quotient = num2 / num1;
     return quotient;
 }
-
